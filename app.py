bcc192e7421f8681a8f290b0afe03d5edeca6c47
ABdroid <aadhirai.b@babajividhyashram.org>
2026-07-26 12:39:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd 
+import requests as rq 
+
+st.set_page_config(
+  page_title = "Bibilog"
+  page_icon = "📚"
+  layout = "wide" 
+)
+
+st.title("📚 Bibilog : Library Management system") 
