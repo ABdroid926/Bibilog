9600d479f96d85b36e66ef4bbe9501ce499beb63
ABdroid <aadhirai.b@babajividhyashram.org>
2026-07-26 12:47:17 +0530

    Fix syntax for set_page_config parameters

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import pandas as pd
 import requests as rq 
 
 st.set_page_config(
-  page_title = "bibilog"
-  page_icon = "📚"
-  layout = "wide" 
+  page_title = "bibilog",
+  page_icon = "📚",
+  layout = "wide" ,
 )
 
 st.title("📚 Bibilog : Library Management system") 
