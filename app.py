fea80851862b9e4f64ce30ddf6f845caf179598a
ABdroid <aadhirai.b@babajividhyashram.org>
2026-08-08 17:12:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ sheet_url = st.secrets["SHEET_URL"]
 
 radiobutton = st.radio("📌 Please do choose an action!",[" 📖 Log In!","✏️ Sign Up !"],horizontal = True) 
 
-if radiobutton == " 📖 Log In!" : 
+if radiobutton == " 📖 Log In!" and st.session_state["logged in"] == "False" : 
   with st.form("LogIn_Page"): 
     user_name = st.text_input("Please do enter your username! :") 
     user_pass = st.text_input("Please do enter your password! :",type = "password") 
@@ -22,9 +22,12 @@ if radiobutton == " 📖 Log In!" :
   if submit: 
     payload = {"action":"login","username": user_name,"password":user_pass}
     response = rq.post(sheet_url,json = payload) 
-    result = response.text
-    st.write(result)
-
+    RESULT = response.text
+    
+    if RESULT == "Success" : 
+      st.session_state["logged in"] == "True" 
+      
+   
 elif radiobutton == "✏️ Sign Up !" : 
     with st.form("SignUp_Page"):
       new_user = st.text_input("Please do enter your username! :")
@@ -40,6 +43,7 @@ elif radiobutton == "✏️ Sign Up !" :
 
       else : 
          st.success("✅ Alright! your new account is created! Please do Log In!!")
+         st.rerun 
       
       
       
