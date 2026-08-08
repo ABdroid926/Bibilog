ce47cdf747737f00b21e37e5b67e8980837f9f1e
ABdroid <aadhirai.b@babajividhyashram.org>
2026-08-08 19:37:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ if st.session_state["logged in"] == "False":
                 st.success("✅ Alright! your new account is created! Please do Log In!!")
                 st.rerun()
 else:
-     if st.session_state["username"] == user_name and st.session_state["is_admin"] == "False" :
+     if st.session_state["username"] = user_name and st.session_state["is_admin"] == "False" :
          st.title(f"Welcome {user_name} !")
          
 
