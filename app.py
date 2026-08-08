4486e92dd7d2cd1d49127be62943b0aff0a1ac85
ABdroid <aadhirai.b@babajividhyashram.org>
2026-08-08 18:01:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,65 @@
 import streamlit as st
-import pandas as pd 
-import requests as rq 
+import pandas as pd
+import requests as rq
 
 st.set_page_config(
-  page_title = "Bibilog",
-  page_icon = "📚",
-  layout = "wide" ,
+    page_title="Bibilog",
+    page_icon="📚",
+    layout="wide",
 )
 
 st.title("📚 Bibilog : Library Management System")
 sheet_url = st.secrets["SHEET_URL"]
 
-if "logged in" not in st.session_state : 
-  st.session_state["logged in"] = "False"
+if "logged in" not in st.session_state:
+    st.session_state["logged in"] = "False"
 
-if "is_admin" not in st.session_state : 
-  st.session_state["is_admin"] = "False"
-  
-if st.session_state["logged in"] == "False" :
-   radiobutton = st.radio("📌 Please do choose an action!",[" 📖 Log In!","✏️ Sign Up !"],horizontal = True) 
+if "is_admin" not in st.session_state:
+    st.session_state["is_admin"] = "False"
 
-if radiobutton == " 📖 Log In!" and st.session_state["logged in"] == "False" : 
-   with st.form("LogIn_Page"): 
-     user_name = st.text_input("Please do enter your username! :") 
-     user_pass = st.text_input("Please do enter your password! :",type = "password") 
-     submit = st.form_submit_button("🚀 Log In!") 
+if st.session_state["logged in"] == "False":
+    radiobutton = st.radio("📌 Please do choose an action!", [" 📖 Log In!", "✏️ Sign Up !"], horizontal=True)
+
+    if radiobutton == " 📖 Log In!":
+        with st.form("LogIn_Page"):
+            user_name = st.text_input("Please do enter your username! :")
+            user_pass = st.text_input("Please do enter your password! :", type="password")
+            submit = st.form_submit_button("🚀 Log In!")
+            
+        if submit:
+            payload = {"action": "login", "username": user_name, "password": user_pass}
+            response = rq.post(sheet_url, json=payload)
+            RESULT = response.text
+            
+            if RESULT == "Success":
+                st.session_state["logged in"] = "True"
+                st.session_state["is_admin"] = "False"
+                st.rerun()
+                
+            elif RESULT == "Admin Success":
+                st.session_state["logged in"] = "True"
+                st.session_state["is_admin"] = "True"
+                st.rerun()
+                
+            else:
+                st.error("❌ Oops, we couldn't find your account. Please check your credentials!!")
     
-if submit: 
-      payload = {"action":"login","username": user_name,"password":user_pass}
-      response = rq.post(sheet_url,json = payload) 
-      RESULT = response.text
-    
-    
-if RESULT == "Success" : 
-       st.session_state["logged in"] = "True"
-       st.session_state["is_admin"] = "False"
-       st.rerun()
-      
-elif RESULT == "Admin Success"  :
-       st.session_state["logged in"] = "True"
-       st.session_state["is_admin"] = "False"
-       st.rerun() 
-      
-else : 
-       st.error("❌ Oops, we could'nt find your account. Please check your credentials!!")
-   
-elif radiobutton == "✏️ Sign Up !" : 
-    with st.form("SignUp_Page"):
-      new_user = st.text_input("Please do enter your username! :")
-      new_pass = st.text_input("Please do enter your password! :",type="password")
-      SUBMIT = st.form_submit_button("🚀 Sign Up!")
-      
-if SUBMIT :   
-      payload = {"action":"register","username":new_user,"password":new_pass}
-      response = rq.post(sheet_url,json = payload)
-      result = response.text 
-if result == "User Exists" : 
-         st.error("❌ Oh dear.. that username is already taken! How about a new one!") 
-
-else : 
-         st.success("✅ Alright! your new account is created! Please do Log In!!")
-         st.rerun 
-      
-      
-      
-
+    elif radiobutton == "✏️ Sign Up !":
+        with st.form("SignUp_Page"):
+            new_user = st.text_input("Please do enter your username! :")
+            new_pass = st.text_input("Please do enter your password! :", type="password")
+            SUBMIT = st.form_submit_button("🚀 Sign Up!")
+            
+        if SUBMIT:
+            payload = {"action": "register", "username": new_user, "password": new_pass}
+            response = rq.post(sheet_url, json=payload)
+            result = response.text
+            
+            if result == "User Exists":
+                st.error("❌ Oh dear.. that username is already taken! How about a new one!")
+            else:
+                st.success("✅ Alright! your new account is created! Please do Log In!!")
+                st.rerun()
  
       
 
