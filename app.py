511291ec94352d3b589a9e3cb7bb4d1e53fe3bb6
ABdroid <aadhirai.b@babajividhyashram.org>
2026-08-08 17:50:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if "logged in" not in st.session_state :
 if "is_admin" not in st.session_state : 
   st.session_state["is_admin"] = "False"
   
-if st.session_state["logged in"] == "False"
+if st.session_state["logged in"] == "False" :
    radiobutton = st.radio("📌 Please do choose an action!",[" 📖 Log In!","✏️ Sign Up !"],horizontal = True) 
 
 if radiobutton == " 📖 Log In!" and st.session_state["logged in"] == "False" : 
