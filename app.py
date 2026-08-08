208a739da0411c884a5fa93e8ef75d5f90645add
ABdroid <aadhirai.b@babajividhyashram.org>
2026-08-08 22:49:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,6 +76,7 @@ else:
          except :
           result = response.text.strip()
              
+          st.write(result)   
          for book in result : 
              with st.container(border= True):
                  st.subheader(book.get("title"))
